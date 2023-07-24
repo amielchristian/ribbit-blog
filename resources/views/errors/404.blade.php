a45c1c4bf27656923ccbc997d11528c07c99ea5e
Amiel Christian Mala-ay <88815448+amielchristian@users.noreply.github.com>
2023-07-24 18:17:51 +0800

    Modify error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,5 +5,11 @@
 @endsection
 
 @section("content")
-    <h1>Error 404!</h1>
+    <a class="error-container" href="{{ route('index') }}">
+        <div class="error-text">
+            <p class='error-message'>Error 404</p>
+            <p class='tip'>Looks like you ran into some trouble. Click here to go back to the homepage.</p>
+        </div>
+        <img class="error-icon-display" src="{{ asset('images/icon.png') }}">
+    </a>
 @endsection
